074ecac1851b9a0458f0ecfc0455cdcf95f42e9b
dariussh77 <dariussh77@gmail.com>
2023-04-11 13:25:50 +0300

    JS Task7

--- JS atsiskaitymas/JS atsiskaitymas/Task 8/script.js
+++ JS atsiskaitymas/JS atsiskaitymas/Task 8/script.js
@@ -5,3 +5,27 @@ subtraction() - priima du skaičius ir grąžina jų skirtumą;
 multiplication() - priima du skaičius ir grąžina jų daugybos rezultatą;
 division() - priima du skaičius ir grąžina jų dalybos rezultatą;
 ------------------------------------------------------------------------------------ */
+
+class Calculator {
+    constructor(sk1, sk2){
+        this.sk1=sk1;
+        this.sk2=sk2;
+    }
+    suma(){
+        return this.sk1+this.sk2;
+    }
+    atimtis(){
+        return this.sk1-this.sk2;
+    }
+    daugyba(){
+        return this.sk1*this.sk2;
+    }
+    dalyba(){
+        return this.sk1/this.sk2;
+    }
+}
+
+console.log(new Calculator(5,7).suma());
+console.log(new Calculator(5,7).atimtis());
+console.log(new Calculator(5,7).daugyba());
+console.log(new Calculator(5,7).dalyba());

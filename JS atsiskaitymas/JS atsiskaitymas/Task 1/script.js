8e1475274808fa28ee7dee01c4c0ba26337d3159
dariussh77 <dariussh77@gmail.com>
2023-04-11 09:25:50 +0300

    JS Task1

--- JS atsiskaitymas/JS atsiskaitymas/Task 1/script.js
+++ JS atsiskaitymas/JS atsiskaitymas/Task 1/script.js
@@ -8,3 +8,48 @@ pamatyti jo pateikto svorio konvertavimą į:
 Pastaba: atvaizdavimas turi būti matomas pateikus formą ir pateikiamas
 <div id="output"></div> viduje, bei turi turėti bent minimalų stilių;
 ------------------------------------------------------------------- */
+
+
+const qfrom=document.querySelector('.wrapper>form');
+qfrom.addEventListener('submit', (e)=>{
+    e.preventDefault();
+    
+    console.log('Eventas:',e);
+    let kintamasis=Number.parseFloat(e.target.elements.search.value);
+    console.log(kintamasis);
+    let lb=kintamasis*2.2046;
+    let gramai=kintamasis*1000;
+    let oz=kintamasis*35.274;
+
+    let qdiv=document.querySelector('#output');
+    qdiv.innerHTML='';
+
+    let divLb=document.createElement('div');
+    let headerLb=document.createElement('h2');
+    let headerLbText=document.createTextNode('Svarais: ')
+    headerLb.appendChild(headerLbText);
+    let resultLb=document.createElement('span');
+    let resultLbText=document.createTextNode(lb);
+    resultLb.appendChild(resultLbText);
+    divLb.append(headerLb,resultLb);
+
+    let divGramai=document.createElement('div');
+    let headerGramai=document.createElement('h2');
+    let headerGramaiText=document.createTextNode('Gramais: ')
+    headerGramai.appendChild(headerGramaiText);
+    let resultGramai=document.createElement('span');
+    let resultGramaiText=document.createTextNode(gramai);
+    resultGramai.appendChild(resultGramaiText);
+    divGramai.append(headerGramai,resultGramai);
+
+    let divOz=document.createElement('div');
+    let headerOz=document.createElement('h2');
+    let headerOzText=document.createTextNode('Uncijomis: ')
+    headerOz.appendChild(headerOzText);
+    let resultOz=document.createElement('span');
+    let resultOzText=document.createTextNode(oz);
+    resultOz.appendChild(resultOzText);
+    divOz.append(headerOz,resultOz);
+
+    qdiv.append(divLb, divGramai, divOz);
+});

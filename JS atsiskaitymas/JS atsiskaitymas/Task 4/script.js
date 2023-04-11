179c61d53a8cb37999c374444ddd1fc7a1dee1f9
dariussh77 <dariussh77@gmail.com>
2023-04-11 11:56:10 +0300

    JS Task4

--- JS atsiskaitymas/JS atsiskaitymas/Task 4/script.js
+++ JS atsiskaitymas/JS atsiskaitymas/Task 4/script.js
@@ -10,3 +10,33 @@ bent minimalų stilių;
 -------------------------------------------------------------------------- */
 
 const ENDPOINT = 'cars.json';
+
+let autoMasyvas
+let qOutput=document.querySelector('#output');
+
+fetch('cars.json')
+    .then(response=>{
+       return response.json();
+    })
+    .then(data=>{
+        console.log(data);
+        data.forEach(e => {
+            let card=document.createElement('div');
+            let ul=document.createElement('ul');
+            let ulText= document.createTextNode(e.brand);
+            ul.append(ulText);
+            card.append(ul);
+
+            e.models.forEach(el=>{
+                let li=document.createElement('li');
+                let liText=document.createTextNode(el);
+                li.appendChild(liText);
+                ul.append(li);
+                card.append(ul);
+            }); 
+            qOutput.append(card);
+            console.log(qOutput);
+        });
+        
+        
+    })

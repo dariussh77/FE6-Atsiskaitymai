8a7e30384b6e7b90f715b9664222b7be41373cd5
dariussh77 <dariussh77@gmail.com>
2023-04-11 13:42:58 +0300

    JS Task9

--- JS atsiskaitymas/JS atsiskaitymas/Task 9/script.js
+++ JS atsiskaitymas/JS atsiskaitymas/Task 9/script.js
@@ -6,3 +6,22 @@ title, director, budget
 Metodas: 
 wasExpensive() - jeigu filmo budget bus didesnis nei 100 000 000 mln USD, tada gražins true, kitu atveju false.
 ------------------------------------------------------------------------------------------------------ */
+
+let movies=[
+    {title:"Avataras", director:'Darius',budget:200000000},
+    {title:"Širvintų grožybės", director:'Živilė',budget:500}
+];
+
+class Movie{
+    constructor(props){
+        this.props=props;
+        return this.brangus();
+    }
+    brangus(){
+        return this.props.budget>=100000000? console.log(true):console.log(false);
+    }
+}
+
+console.log(new Movie(movies[0]));
+console.log('------------------------------------------');
+console.log(new Movie(movies[1]));
